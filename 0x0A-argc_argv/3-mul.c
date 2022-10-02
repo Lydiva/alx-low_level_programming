a3d2ca65c7b174b72c6d2d53a2d01d55ca8a3493
root <goldugo04@yahoo.com>
2022-10-02 16:38:41 -0700

    Task 3 done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,7 +12,6 @@
 int main(int argc, char *argv[])
 {
 	int a, b;
-
 	if (argc == 3)
 	{
 		a = atoi(argv[1]);
@@ -21,5 +20,6 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 	printf("Error\n");
+
 	return (1);
 }
