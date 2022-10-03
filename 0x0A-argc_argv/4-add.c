14c171c5cfcb67db820e742da6baa5aad827cc2e
root <goldugo04@yahoo.com>
2022-10-02 17:21:40 -0700

    Task 4 done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,4 @@
-#include <stdio.h>                                                  
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
@@ -12,19 +12,19 @@
 
 int check_num(char *str)
 {
-	/*Declaring variables*/
-	unsigned int count;
-	
-	count = 0;
-	while (count < strlen(str)) /*count string*/
-	
+/*Declaring variables*/
+unsigned int count;
+
+count = 0;
+while (count < strlen(str)) /*count string*/
+
 	{
 		if (!isdigit(str[count])) /*check if str there are digit*/
 		{
 			return (0);
 		}
-		
-		count++;
+
+	count++;
 	}
 	return (1);
 }
@@ -39,22 +39,22 @@ int check_num(char *str)
 
 int main(int argc, char *argv[])
 {
-	/*Declaring variables*/
-	int count;
-	int str_to_int;
-	int sum = 0;
+/*Declaring variables*/
+int count;
+int str_to_int;
+int sum = 0;
 	
-	count = 1;
-	while (count < argc) /*Goes through the whole array*/
+count = 1;
+while (count < argc) /*Goes through the whole array*/
 	{
 		if (check_num(argv[count]))
-		
+
 		{
 			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 			sum += str_to_int;
 		}
 
-		/*Condition if one of the number contains symbols that are not digits*/
+	/*Condition if one of the number contains symbols that are not digits*/
 		else
 		{
 			printf("Error\n");
