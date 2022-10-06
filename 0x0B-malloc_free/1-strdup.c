64613e35d0b2d0665a0a00bde46662ff075794ca
root <goldugo04@yahoo.com>
2022-10-05 21:02:18 -0700

    Task 1 completed

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -21,15 +21,15 @@ char *_strdup(char *str)
 	for (index = 0; str[index]; index++)
 		len++;
 	cpy = malloc(sizeof(char) * (len + 1));
-	
+
 	if (cpy == NULL)
 		return (NULL);
-	
+
 	for (index = 0; str[index]; index++)
 	{
 		cpy[index] = str[index];
 	}
-	
+
 	cpy[len] = '\0';
 
 	return (cpy);
